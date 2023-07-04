03d065501e75b8d3bd0bae472d0703acec5f1f96
Sugan T <t.suganeeshwaran@gmail.com>
2023-07-05 00:30:19 +0530

    added two files for helper func and config

--- src/js/controller.js
+++ src/js/controller.js
@@ -3,14 +3,6 @@ import recipeview from './view/recipeview.js';
 import 'core-js/stable';
 import 'regenerator-runtime';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,4 +1,6 @@
 import { async } from 'regenerator-runtime';
+import { API_URL } from './config';
+import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
@@ -6,15 +8,12 @@ export const state = {
 
 export const loadRecipe = async function (id) {
   try {
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}?`
-    );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message}(${res.status})`);
-    console.log(res, data);
-    let { recipe } = data.data;
+    const data = await getJSON(`${API_URL}/${id}`);
+
+    const { recipe } = data.data;
+
     state.recipe = {
-      id: recipe.id,  
+      id: recipe.id,
       title: recipe.title,
       imageUrl: recipe.image_url,
       publisher: recipe.publisher,
@@ -23,8 +22,9 @@ export const loadRecipe = async function (id) {
       cookingTime: recipe.cooking_time,
       ingredients: recipe.ingredients,
     };
+
     console.log(state.recipe);
   } catch (err) {
-    alert(err);
+    console.error(`${err} 💥💥💥`);
   }
 };
